baa4645e1e2d91374e3f5dac47ddfe7977e185a4
Agnaldo Aquino <agnaldo.aquino@gmail.com>
2025-06-17 00:42:04 -0300

    Correção de bug na importação

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from datetime import datetime, timedelta
 from jwt.exceptions import InvalidTokenError
 from uuid import UUID
 from src.models import AcessoCreate, AcessoUpdate
-from src.models import EmailCreate, EmailUpdate
+
 
 load_dotenv()
 
